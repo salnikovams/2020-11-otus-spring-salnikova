8d38464a9f74716cc557d764b96b81db0c4eee76
salnikovams <75277252+salnikovams@users.noreply.github.com>
2021-03-03 22:59:45 +0300

    mvc view

---
+++ spring-09-MVC-VIEW/src/main/java/ru/otus/spring/service/LibraryService.java
@@ -0,0 +1,24 @@
+package ru.otus.spring.service;
+
+import ru.otus.spring.domain.Author;
+import ru.otus.spring.domain.Book;
+import ru.otus.spring.domain.Comment;
+import ru.otus.spring.domain.Genre;
+
+import java.util.List;
+
+public interface LibraryService {
+
+    public Book addBook(String bookName, String authorName, String genreName);
+    public Book updateBookInfo(Long id, String bookName);
+    public void deleteBook(Long id);
+    public Book getBookById(Long id);
+    public Book getBookByName(String name);
+    public List<Book> getAllBooks();
+    public void deleteAuthor(Long id);
+    public void deleteGenre(Long id);
+    public Author getAuthorByName(String name);
+    public Genre getGenreByName(String name);
+    public void addComment(Long bookId,  String comment);
+    public List<Comment> getCommentsByBook(Long bookId);
+}
